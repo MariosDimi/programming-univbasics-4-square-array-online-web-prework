7386c27895eb43c630c88cfdc380eec200325bf8
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-24 23:10:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  numbers = [2,3,4]
+  counter = 0
+  while square_array(numbers)
+    puts array[counter]
+    counter +=1 
+  end
 end
